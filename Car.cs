70c7809ad3ca7391ed996e5b4bce761b1b5e09c2
Claudio Ponczek <129533037+ooduk@users.noreply.github.com>
2023-03-31 18:09:28 -0300

    Add files via upload
    Spawner de carros e movimentação em apenas uma direção

---
+++ Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    
+    public int direction;
+    public float limit;
+
+    public float speed = 2;
+
+    private void Update()
+    {
+
+        transform.position += Vector3.right * direction * speed * Time.deltaTime;
+
+        if (direction >= -5 && transform.position.x > limit)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] _veiculo;
+
+    private float _timeBtwSpawn;
+    public float _startTimeBtwSpawn;
+    public float _decreaseTime;
+    public float _minTime = 0.65f;
+
+    private void Update()
+    {
+        if (_timeBtwSpawn <= 0)
+        {
+            int r = Random.Range(0, _veiculo.Length);
+
+            Instantiate(_veiculo[r]);
+            _timeBtwSpawn = _startTimeBtwSpawn;
+            if (_startTimeBtwSpawn > _minTime)
+            {
+                _startTimeBtwSpawn -= _decreaseTime;
+            }
+        }
+        else
+        {
+            _timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
